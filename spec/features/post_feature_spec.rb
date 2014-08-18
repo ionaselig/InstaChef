4e63ee105d488084f4995ab00cb0500a5183f730
ionaselig <you@example.com>
2014-08-18 12:32:23 +0100

    features added

--- spec/features/post_feature_spec.rb
+++ spec/features/post_feature_spec.rb
@@ -12,8 +12,21 @@ describe 'posts' do
 		before do
 			Post.create(title: 'Bacon')
 		end
+ 	
+		it 'desplay a list of all posts' do
+			visit '/posts'
+			expect(page).to have_content 'Bacon'
+		end
  	end
 
+ 	describe 'creating posts' do
+ 		it 'adds posts from a form' do
+		visit '/posts'
+		click_link 'Deposit Food Porn'
+		fill_in 'Title', with: 'A brand new post'
 
-
+		expect(page).to have_content 'A brand new post'
+		expect(current_path).to eq '/posts'
+		end 
+	end 
 end
