8bd00912366bd56f955a111b6bdc12dba852a19b
ionaselig <you@example.com>
2014-08-19 11:11:44 +0100

    tagging feature file and post.rb spec tests written

---
+++ spec/features/tagging_feature_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+# describe 'tagging posts' do 
+# 	it tags as links under posts do
+# 		visit '/posts'
+# 		clink_link 'Create New Food Porn'
+# 		fill_in 
+	
+end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,3 +3,33 @@ require 'rails_helper'
 RSpec.describe Post, :type => :model do
   pending "add some examples to (or delete) #{__FILE__}"
 end
+
+
+# describe '#tag_list=' do
+# 	let(:post) { Post.create(title: 'Example post')}
+
+
+# 	context 'no tags provided' do
+# 		it 'gives 0 tags to the post' do
+# 			post.tag_list = ''
+# 			expect(post.tags).to be_empty
+# 		end 
+# 	end
+
+# 	context '1 tag' do
+# 		it 'adds that tag to our post' do
+# 			post.tag_list = '#yolo'
+# 			expect(post.tags.first.text).to eq '#yolo'
+# 		end 
+# 	end
+
+# 	context 'multiple tags' do
+# 		it 'adds all the tags to our posts' do
+# 			post.tag_list = '#yolo, #swag'
+# 			expect(post.tags.first.text).to eq '#yolo'
+# 		end 
+# 	end	
+
+# 	context 'multiple tags' do
+# 		before { Tag.create(text: '#yolo')}
+# end
