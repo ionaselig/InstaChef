0a8a3bf9b1d9dbc85143d35dc2f2a62cd9005db2
Talal Khodr <Talal.Khodr@Gmail.com>
2014-08-20 12:17:20 +0100

    Added Pretty URLS for tags

--- spec/features/tagging_feature_spec.rb
+++ spec/features/tagging_feature_spec.rb
@@ -1,14 +1,36 @@
 require 'rails_helper'
 
-describe 'tagging posts' do 
-	it 'displays tags as links under posts' do
-		visit '/posts'
-		click_link 'Deposit Food Porn'
-		fill_in 'Title', with: 'A brand new post'
-		fill_in 'Tags', with: '#yolo, #swag'
-		click_button 'Create New Food Porn'
+describe 'tagging posts' do
+  it 'displays tags as links under posts' do
+    visit '/posts'
+    click_link 'Deposit Food Porn'
+    fill_in 'Title', with: 'A brand new post'
+    fill_in 'Tags', with: '#yolo, #swag'
+    click_button 'Create New Food Porn'
 
-		expect(page).to have_link '#yolo'
-		expect(page).to have_link '#swag'
-	end
+    expect(page).to have_link '#yolo'
+    expect(page).to have_link '#swag'
+  end
+end
+
+describe 'filtering by tags' do
+  before do
+    Post.create(title: 'Post A', tag_list: '#yolo, #swag')
+    Post.create(title: 'Post B', tag_list: '#yolo, #bob')
+  end
+
+  it 'filters to show only tagged posts' do
+    visit '/posts'
+    click_link '#swag'
+
+    expect(page).to have_css 'h1', text: 'Posts tagged with #swag'
+    expect(page).to have_content 'Post A'
+    expect(page).not_to have_content 'Post B'
+  end
+
+  it 'accessible via pretty URLs' do
+    visit '/tags/swag' 
+
+    expect(page).to have_content 'Post A'
+  end
 end
